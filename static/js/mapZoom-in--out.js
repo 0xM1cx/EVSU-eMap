acdc914b49fc1b218594a4e7c9a6fedd4e2447e8
Charles Lance <lancegula05@gmail.com>
2024-01-27 15:46:09 +0800

    Working on Search Bar and EVSU Map Facilities

--- static/js/mapZoom-in--out.js
+++ static/js/mapZoom-in--out.js
@@ -1,19 +1,25 @@
-// Zoom-in-Zoom-Out function for the map
-
-let currentZoom = 1;
-
-function zoomIn() {
-    currentZoom += 0.2;
-    applyZoom();
-}
-
-function zoomOut() {
-    currentZoom -= 0.2;
-    applyZoom();
-}
-
-
-function applyZoom() {
-    const map = document.querySelector('.map');
-    map.style.transform = `scale(${currentZoom})`;
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var zoomIn = document.querySelector('#zoom-in');
+    var zoomOut = document.querySelector('#zoom-out');
+    let defaultWidth = 700;
+  
+    zoomIn.addEventListener('click', function() {
+      defaultWidth += 150;
+      applyZoom();
+    });
+  
+    zoomOut.addEventListener('click', function() {
+      defaultWidth -= 150;
+      applyZoom();
+    });
+  
+    function applyZoom() {
+      var evsuMap = document.querySelector('#evsu_map');
+      evsuMap.style.width = `${defaultWidth}px`;
+    }
+    document.addEventListener('wheel', function(e) {
+        if (e.ctrlKey || e.metaKey) {
+          e.preventDefault();
+        }
+      });
+  });
