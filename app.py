c6d9470407a61a469d27b5ca03a8e4b6fa1a1ef4
/Code.Byte <https://github.com/JohnIvanGalang>
2024-01-27 12:43:51 +0800

    EMAP UPDATED

--- app.py
+++ app.py
@@ -8,67 +8,46 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'kunwariSecretLang'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///maps_user.db'
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
-class User(db.model):
-    __table__ = 'User_Table'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(200), nullable=False)
-    last_name = db.Column(db.String(200), nullable=False)
-    age = db.Column(db.String(200), nullable=False)
-    address_name = db.Column(db.String(200), nullable=False)
+# class User(db.model):
+#     __table__ = 'User_Table'
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(200), nullable=False)
+#     username = db.Column(db.String(200), nullable=False)
+#     password = db.Column(db.String(200), nullable=False)
 
 
-class RegistrationForm(FlaskForm):
-    First_Name = StringField('First Name', validators=[DataRequired(), Length(min=3, max=100)])
-    Last_Name = StringField('Last Name', validators=[DataRequired(), Length(min=3, max=100)])
-    Age = StringField('Age', validators=[DataRequired(), Length(min=3, max=100)])
-    Address = StringField('Age', validators=[DataRequired(), Length(min=3, max=100)])
-    submit = SubmitField('Sign Up')
+# class RegistrationForm(FlaskForm):
+#     email = StringField('Email', validators=[DataRequired(), Length(min=3, max=100)])
+#     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=100)])
+#     password = StringField('Password', validators=[DataRequired(), Length(min=3, max=100)])
 
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Log In')
+# class LoginForm(FlaskForm):
+#     email = StringField('Email', validators=[DataRequired()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     submit = SubmitField('Log In')
 
 
-app.route('/registration', methods=['POST', 'GET'])
+# app.route('/', methods=['POST'])
+# def register():
+#     form = RegistrationForm()
+
+#     if form.validate_on_submit():
+#        pass
+
+#     return render_template('login.html', form=form)
 
 
-def register():
-    form = RegistrationForm()
-
-    if form.validate_on_submit():
-        FirstName = form.First_Name.data
-        LastName = form.Last_Name.data
-        Age = form.Age.data
-        Address = form.Address.data
-        new_User = User(First_Name=FirstName, Last_Name=LastName, Age=Age, Address=Address)
-        db.session.add(new_User)
-        db.session.commit()
-        flash('Account created successfully!', 'success')
-        return redirect(url_for('landing'))
-
-    return render_template('index.html', form=form)
-
-
-app.route('/login', methods=['POST', 'GET'])
-
-
+@app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('login.html')
 
-
-app.route('/', methods=['POST', 'GET'])
-
-
+@app.route('/home')
 def landing():
-    return render_template('index.html')
-
-
-app.route
+    return render_template('main.html')
 
 if __name__ == '__main__':
     # with app.app_context():
